6ea5a6f651d7a953e4ccdbcccfb0898e49a44e37
Larissa <larissa.vs@aluno.ufop.edu.br>
2021-06-23 19:42:42 -0300

    feat: include sidebar menu

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,25 +1,35 @@
-import { Divider } from '@material-ui/core'
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import './styles.css'
 
-
 import photo from './../../_assets/athlete-image.png'
 import Card from './../../_components/cards'
+import Header from './../../_components/header'
+
+import Sidebar from '../../_components/burger-menu/Sidebar'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function InitialView(){
     return (
-        <div className='home-container'>
-            <Card 
-                id="2458795"
-                photo={photo}
-                name="Lucas Morais Costa"
-                date="01/12/1994"
-                position="Goleiro"
-                club="Atlético Mineiro"
-            />
-        </div>
-
-    )
+        <Router>
+        <Sidebar />
+        <Switch>
+        </Switch>
+      </Router>
+    );
 }
+
+      /*</div>
+            <Header/>
+            <div className="cards">
+                <Card 
+                    id="2458795"
+                    photo={photo}
+                    name="Lucas Morais Costa"
+                    date="01/12/1994"
+                    position="Goleiro"
+                    club="Atlético Mineiro"
+                />
+            </div>
+            </div>*/
