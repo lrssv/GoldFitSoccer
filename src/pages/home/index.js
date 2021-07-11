973885ff4c2debf64d9726559d8b6f302d4ef925
Larissa <larissa.vs@aluno.ufop.edu.br>
2021-07-11 14:36:27 -0300

    feat: table add

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,17 +8,48 @@ import Header from './../../_components/header'
 
 import Sidebar from '../../_components/burger-menu/Sidebar'
 
+import Tabs from './../../_components/table/tabs'
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export default function InitialView(){
     return (
+        <div>
+          <h1>Potencial Esportivo</h1>
+          <div className="container-home">
+          <Tabs>
+            <div label="Potencial Esportivo">
+              See ya later, <em>Alligator</em>!
+            </div>
+            <div label="Fatores Antropométricos">
+              After 'while, <em>Crocodile</em>!
+            </div>
+            <div label="Fatores Fisico-motores">
+              Nothing to see here, this tab is <em>extinct</em>!
+            </div>
+            <div label="Maturação Biológica">
+              See ya later, <em>Alligator</em>!
+            </div>
+            <div label="Apoio dos Familiares">
+              After 'while, <em>Crocodile</em>!
+            </div>
+            <div label="Habilidades Táticas">
+              Nothing to see here, this tab is <em>extinct</em>!
+            </div>
+          </Tabs>
+        </div>
+        </div>
+      );
+}
+
+/* return (
         <Router>
         <Sidebar />
         <Switch>
         </Switch>
       </Router>
     );
-}
+    /*
 
       /*</div>
             <Header/>
